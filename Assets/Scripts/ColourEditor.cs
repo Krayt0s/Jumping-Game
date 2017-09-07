4f78c8e57f3a86369ddd82c30cf9c42ef747bf6c
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2017-09-07 04:06:35 +0100

    Convenience script for water colour changes.

---
+++ Assets/Scripts/ColourEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ColourEditor : MonoBehaviour {
+    [Range(0, 255)]
+    public int alpha = 1;
+    private int lastAlpha = 1;
+	
+	void Update () {
+		if(alpha != lastAlpha) {
+            lastAlpha = alpha;
+            float a = (float) alpha / 255;
+            foreach(var sr in GetComponentsInChildren<SpriteRenderer>()) {
+                Color c = sr.color;
+                c.a = a;
+                sr.color = c;
+            }
+        }
+	}
+}
