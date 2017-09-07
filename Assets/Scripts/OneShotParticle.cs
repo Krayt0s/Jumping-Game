d0af6eb7601e57313dad32ce5c482c09d6da79c5
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2017-09-07 04:03:20 +0100

    Added a splash effect particle system.

---
+++ Assets/Scripts/OneShotParticle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class OneShotParticle : MonoBehaviour {
+	void Start () {
+        var ps = GetComponent<ParticleSystem>();
+        Invoke("Finish", ps.main.duration + ps.main.startLifetime.constantMax + 0.1f);
+	}
+
+    private void Finish() {
+        Destroy(gameObject);
+    }
+}
