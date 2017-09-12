efb137cc15f0873a5e42e39b17bfdd899ed90a33
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2017-09-12 19:16:14 +0100

    Added fly collection sound

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -2,13 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LandingController))]
 public class Collector : MonoBehaviour {
+    public AudioClip eatSound;
+
+    private LandingController lc;
     private float points = 0;
+
+    void Awake() {
+        lc = GetComponent<LandingController>();
+    }
 	
 	void OnTriggerEnter2D(Collider2D coll) {
-        if (coll.gameObject.layer == LayerMask.NameToLayer("Collectable")) {
-            Destroy(coll.gameObject);
-            points++;
+        if(coll.gameObject.layer != LayerMask.NameToLayer("Collectable")) {
+            return;
+        }
+
+        if(lc.submerged) {
+            // Nothing yet is collectable underwater
+        } else {
+            if (true) {
+                Destroy(coll.gameObject);
+                AudioSource.PlayClipAtPoint(eatSound, transform.position);
+                points++;
+            }
         }
     }
 }
