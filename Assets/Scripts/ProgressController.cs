66ca9d7d431d1e745f999f34adfe2f0c6060ad76
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2017-09-13 00:11:39 +0100

    Set up script for tracking level progress.

---
+++ Assets/Scripts/ProgressController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressController : MonoBehaviour {
+    public GameObject progressor;
+    private float maxY = 0;
+	
+	void Update () {
+        maxY = Mathf.Max(progressor.transform.position.y, maxY);
+        float dy = Mathf.Max(maxY - transform.position.y, 0);
+        transform.position += (Vector3) new Vector2(0, dy);
+	}
+}
